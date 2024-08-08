477e024a084a695b7d8bb591814a346d3eff146d
Sailesh Kumar <saileshkumar.gunasekaran@@lr.org>
2024-08-08 16:22:43 +0800

    feat: Implemented feature 1

--- src/calculator/calculator.ts
+++ src/calculator/calculator.ts
@@ -1,5 +1,21 @@
 const add = (stringExp: string) => {
-    return 
+  if (typeof stringExp === 'undefined') {
+    throw new Error('Input is undefined');
+  }
+    
+  if (stringExp === '') {
+    return 0;
+  }
+    
+  const numbers = stringExp.split(',').map(num => {
+    const parsed = parseFloat(num);
+    if (isNaN(parsed)) {
+      throw new Error(`Invalid number: ${num}`);
+    }
+    return parsed;
+  });
+    
+  return numbers.reduce((sum, num) => sum + num, 0); 
 }
 
-export default {add}
+export default { add }
